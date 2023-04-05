bce13e18d550076f3de524fd115b245a251aff62
jarltuxen <jarl.tuxen@gmail.com>
2023-04-05 12:24:36 +0200

    Coding Challenge 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+// let js = 'amazing';
+// //if (js === 'amazing') alert('Javascript is FUN!');
+// console.log(40 + 2);
+// let PI = 3.1415265;
+// let country = "Denmark";
+// let continent = "Europe";
+// let population = 6500000;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//Codeing Challenge #1
+
+// const heightMark = 1.69;
+// const weightMark = 78;
+// const heightJohn = 1.95;
+// const weightJohn = 92;
+const heightMark = 1.88;
+const weightMark = 95;
+const heightJohn = 1.76;
+const weightJohn = 85;
+const bmiMark = weightMark / heightMark ** 2;
+const bmiJohn = weightJohn / heightJohn ** 2;
+const markHigherBMI = bmiMark > bmiJohn;
+console.log(bmiMark, bmiJohn, markHigherBMI);
